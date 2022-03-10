22e330615aba3c1654fc01f1151057180eb1fc35
Kainoa Kanter <kainoa@t1c.dev>
2022-03-09 18:00:40 -0800

    🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿

--- sussy.js
+++ sussy.js
@@ -3,3 +3,9 @@ document.getElementById("gay").addEventListener("click", () => {
     const newAudio = origAudio.cloneNode()
     newAudio.play();
 });
+
+document.getElementById("stinky").addEventListener("click", () => {
+    const origAudio = document.getElementById("sfx2");
+    const newAudio = origAudio.cloneNode()
+    newAudio.play();
+});
