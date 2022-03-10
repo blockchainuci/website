cd185a2b883789cea319ce5cebb0945b3b5d7c31
Kainoa Kanter <kainoa@t1c.dev>
2022-03-09 18:14:10 -0800

    CSS shit idk

--- sussy.js
+++ sussy.js
@@ -1,11 +1,13 @@
-document.getElementById("gay").addEventListener("click", () => {
-    const origAudio = document.getElementById("sfx");
-    const newAudio = origAudio.cloneNode()
-    newAudio.play();
-});
+window.addEventListener("DOMContentLoaded", event => {
+    document.getElementById("gay").addEventListener("click", () => {
+        const origAudio = document.getElementById("sfx");
+        const newAudio = origAudio.cloneNode()
+        newAudio.play();
+    });
 
-document.getElementById("stinky").addEventListener("click", () => {
-    const origAudio = document.getElementById("sfx2");
-    const newAudio = origAudio.cloneNode()
-    newAudio.play();
+    document.getElementById("stinky").addEventListener("click", () => {
+        const origAudio = document.getElementById("sfx2");
+        const newAudio = origAudio.cloneNode()
+        newAudio.play();
+    });
 });
